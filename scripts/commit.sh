e23ee8f3014a681f9e4c43979b2f6579a921897e
Hai Liang Wang <hai@chatopera.com>
2026-01-19 09:34:23 +0800

    Update contents

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -14,12 +14,12 @@ export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 git add --all
 git commit -m "Update contents"
-git pull origin master -ff
+git pull origin main -ff
 
 if [ ! $? -eq 0 ]; then
     echo "Failed, fix manullay"
     exit 1
 fi
 
-git push origin master
+git push origin main
 echo "Updated contents"
